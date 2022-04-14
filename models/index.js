59abbf4bd5c7695be1986231d0844b5db69020d7
ibafly <copppe@gmail.com>
2022-04-14 19:20:00 +0800

    part13 13.19 add blogs-users connection, the reading list

--- models/index.js
+++ models/index.js
@@ -1,9 +1,13 @@
 const Blog = require("./blog")
 const User = require("./user")
+const ReadingList = require("./readingList")
 
 User.hasMany(Blog)
 Blog.belongsTo(User)
 // User.sync({ alter: true })
 // Blog.sync({ alter: true })
+
+User.belongsToMany(Blog, { through: ReadingList, as: "reading" })
+Blog.belongsToMany(User, { through: ReadingList })
 
 module.exports = { Blog, User }
---
+++ models/readingList.js
@@ -0,0 +1,32 @@
+const { Sequelize, QueryTypes, DataTypes, Model } = require("sequelize")
+const { sequelize } = require("../utils/db")
+
+class ReadingList extends Model {}
+ReadingList.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    userId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: { model: "users", key: "id" },
+    },
+
+    blogId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: { model: "blogs", key: "id" },
+    },
+  },
+  {
+    sequelize, // instance object created from database data
+    underscored: true,
+    timestamps: false,
+    modelName: "readingList",
+  }
+)
+
+module.exports = ReadingList
